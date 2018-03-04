3d30febef3e5aaf8dd8d1d84f6c357cb3c76515c
Raymond Cano <rfc1@rice.edu>
2018-03-04 12:50:05 -0800

    getting started with cleaning data

--- scrape/request.py
+++ scrape/request.py
@@ -6,54 +6,73 @@ import os
 import datetime
 import math
 
+
 lyric_segment_annotation = re.compile("\[.*\]")
 
 base_url = "https://api.genius.com"
 
 token = os.environ['GENIUS_TOKEN']
+MAX_SONGS_PER_REQUEST = 50
 
-def download_lyrics(artist):
-    artist_id = 16775   #TODO: Make name -> ID Mapping
-    songs = _get_songs_for_artist(artist_id)
-    _write_songs(songs, artist)
+f = open('./db/artist_maps/1519639336.json', 'r')
+ARTIST_MAP = json.load(f)
+f.close()
+
+
+def download_lyrics(artist, number_of_songs=20):
+    artist_id = ARTIST_MAP[artist]
+    page = 100
+    songs_per_request = min(number_of_songs, MAX_SONGS_PER_REQUEST)
+    while number_of_songs > 0:
+        songs = _get_songs_for_artist(artist_id, songs_per_request, page)
+        page += 1
+        number_of_songs -= songs_per_request
+        _write_songs(songs, artist)
+
 
 def _write_songs(songs, artist):
+    print("writing to file")
     artist_path = './db/{}'.format(artist)
     os.makedirs(artist_path, exist_ok=True)
-    for idx, song in enumerate(songs):
-        with open(os.path.join(artist_path + '/{}.txt'.format(idx)), 'w') as f:
-            f.write(song)
+    for idx, (title, lyrics) in enumerate(songs):
+        modified_title = title.replace('/', '_').replace(' ', '_')
+        with open(os.path.join(artist_path + '/{}_{}.txt'.format(modified_title, idx)), 'w') as g:
+            g.write(lyrics)
     return
 
 
-def _get_songs_for_artist(artist_id, number_of_songs=20):
+def _get_songs_for_artist(artist_id, number_of_songs, page):
     """
-    :param artist_id:
-    :param number_of_songs:
-    :return:
+    :param artist_id: Genius artist ID
+    :param number_of_songs: Number of songs to request
+    :return: List of tuples with the title and the lyrics, each as strings
     """
+    titles_and_lyrics = []
+
     response = requests.request(
         "GET",
         "{}/artists/{}/songs".format(base_url, artist_id),
         headers={"Authorization": "Bearer {}".format(token)},
-        params={"per_page": number_of_songs}
+        params={"per_page": number_of_songs, "page": page}
     )
     if response.status_code != 200:
         raise Exception("Bad Response: \nCode: {}\n Content:{}".format(response.status_code, response.content))
 
     response_content = json.loads(response.content)
     songs = response_content['response']['songs']
-    lyrics = []
+
     for song in songs:
         print('getting song for url: {}'.format(song['url']))
-        lyrics.append(_get_lyrics_for_url(song['url']))
-    return lyrics
+        titles_and_lyrics.append((song['title'], _get_lyrics_for_url(song['url'])))
 
-def make_artist_map(start=1, max=20000):
+    return titles_and_lyrics
+
+
+def make_artist_map(start=1, end=20000):
     map = {}
     map_path = 'db/artist_maps/{}.json'.format(math.floor(datetime.datetime.now().timestamp()))
     os.makedirs('db/artist_maps', exist_ok=True)
-    for i in range(start, max):
+    for i in range(start, end):
         name, err = _get_artist_name(i)
         if err is not None:
             continue
@@ -93,8 +112,12 @@ def _get_lyrics_for_url(url):
     for comment_element in lyrics_div(text=lambda text: isinstance(text, Comment)):
         comment_element.extract()
     lines = []
+    last = -1
     for c in lyrics_div.descendants:
-        if c.string is not None:
+        if c.string is not None and last != c.previous_sibling:
             lines.append(c.string.strip())
-    filtered_lines = filter(lambda text: text != '' and lyric_segment_annotation.match(text) is None, lines)
+            # print(c.tag)
+            last = c
+
+    filtered_lines = filter(lambda text: text != '', lines)
     return '\n'.join(list(filtered_lines))
